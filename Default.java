d8c44aed5217e06797a5cb3ed78563b9ab8519dc
juliaawestman <juliawestman21@gmail.com>
2015-11-26 18:33:04 +0100

    Added TCross + Default + PathTile + TileTest

---
+++ Default.java
@@ -0,0 +1,12 @@
+import java.awt.image.BufferedImage;
+
+public class Default extends PathTile {
+
+    private BufferedImage img;
+    
+    public Default(BufferedImage pathImg) {
+	img = pathImg;
+    }
+
+    
+}
---
+++ TCross.java
@@ -0,0 +1,11 @@
+import java.awt.image.BufferedImage;
+
+public class TCross extends PathTile {
+    
+    private BufferedImage img;
+    
+    public TCross(BufferedImage pathImg) {
+   	img = pathImg;
+       }
+
+}
