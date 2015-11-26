30335455634c826bd574b6927bb2695353a3c2a9
dv14emm <dv14emm@cs.umu.se>
2015-11-26 11:54:08 +0100

    Started

--- src/Tower.java
+++ src/Tower.java
@@ -1,1 +1,25 @@
+/**
+ * Class:       Tower
+ *
+ * Author:      Erik Moström
+ * cs-user:     dv14emm
+ * Date:        2015-11-26
+ */
 
+package src;
+
+public abstract class Tower {
+
+    private int range;
+    private int speed;
+    private int damage;
+    private boolean ground;
+    private boolean air;
+    private Unit target;
+    private int timeOfLastAttack;
+
+    /*TODO testar todo grejen*/
+    public boolean withinRange(Unit newTarget){
+        return false;
+    }
+}
---
+++ src/TowerTest.java
@@ -0,0 +1,13 @@
+package src;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TowerTest {
+
+    @Test
+    public void shouldReturnTrueForSamePos() throws Exception {
+
+    }
+}
