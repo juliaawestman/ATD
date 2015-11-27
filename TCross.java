8393ffcd82498ac49296f3a7736308ec91f0f65b
juliaawestman <juliawestman21@gmail.com>
2015-11-27 13:37:07 +0100

    TCross update

--- TCross.java
+++ TCross.java
@@ -1,12 +1,54 @@
-import java.awt.image.BufferedImage;
+/*
+ * TCross
+ * 
+ * Date 27/11- 2015
+ * @author id12jwn
+ */
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * TCross a class that extends the PathTile class. The class
+ * have a own image that will be used in the user interface.   
+ */
 public class TCross extends PathTile {
     
-    //TODO: ändra url till den riktiga 
-    private static final String IMG_URL = "http://www.leksand.se/Images/Bygga,%20bo%20och%20miljö/Djur/Katt2.jpg";
+    private static String PATH ="TCross.jpg"; 
     
+    private BufferedImage img = null;
+    
+    /**
+     * Default is the constructor that will read in the image when 
+     * the program will create a object of the class Default
+     */
     public TCross() {
-
-       }
+	readInImg();
+    }
+    
+    /**
+     * ReadInImg is a method that will read in a image that 
+     * will be used in the user interface
+     */
+    public void readInImg(){
+	try {
+	   // URL url = new URL(IMG_URL);
+	    File file = new File(PATH);
+	    img = ImageIO.read(file);
+	    
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+    
+    /**
+     * getImage is method that will get the image
+     * @return a BufferedImage
+     */
+    public BufferedImage getImg(){
+	return img;
+    }
 
 }
