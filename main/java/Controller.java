7b7069d0734ccf2705f134290b55c9fba0ad1831
dv14emm <dv14emm@cs.umu.se>
2015-12-09 15:01:40 +0100

    Started on a controller

---
+++ main/java/Controller.java
@@ -0,0 +1,50 @@
+package main.java;
+
+import main.java.GUI.CLayout;
+import main.java.GUI.MapInformation;
+import main.java.tile.Tile;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Class:       Controller
+ * <p/>
+ * Author:      Erik Moström
+ * cs-user:     dv14emm
+ * Date:        12/9/15
+ */
+public class Controller implements MapInformation {
+    private MapFactory factory;
+
+    public Controller (MapFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public ArrayList<String> getLevelName() {
+        return factory.getMapNames();
+    }
+
+    @Override
+    public HashMap<Position, Tile> getMap(String s) {
+        return factory.loadMap(s).getCompleteMap();
+    }
+
+    public static void main(String[] args) {
+        MapFactory f = null;
+        try {
+            f = new MapFactory("main/resources/defaultMaps.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        Controller c = new Controller(f);
+
+        new CLayout(c);
+    }
+}
